f21cea4fa07413e30b339641985623aae9c322f6
PvasquezF <201503611@ingenieria.usac.edu.gt>
2020-01-01 17:05:23 -0600

    Se agregaron los clientes de la app2 y app3

--- server.js
+++ server.js
@@ -4,6 +4,21 @@ const app = express();
 const request = require('request');
 const url = 'https://92rjxexbvf.execute-api.us-east-1.amazonaws.com/get/getCharacters';
 
+var PROTO_PATH = __dirname + '/hello.proto';
+var grpc = require('grpc');
+var protoLoader = require('@grpc/proto-loader');
+var packageDefinition = protoLoader.loadSync(
+    PROTO_PATH, {
+        keepCase: true,
+        longs: String,
+        enums: String,
+        defaults: true,
+        oneofs: true
+    });
+var hello_proto = grpc.loadPackageDefinition(packageDefinition).helloworld;
+var client = new hello_proto.Greeter('localhost:50051', grpc.credentials.createInsecure());
+var client2 = new hello_proto.Greeter('localhost:50052', grpc.credentials.createInsecure());
+var user;
 
 app.use(express.static(__dirname + '/css'));
 app.set("view engine", "ejs");
@@ -13,8 +28,36 @@ app.listen(3000, () => {
 app.get('/', (req, res) => {
     request(url, function(error, response, body) {
         body = JSON.parse(body);
-        res.render('index', {
-            "data": body
+        user = req.params.id;
+        client.sayHello({ name: 'Hola Mundo' }, function(err, responseA2) {
+            client2.sayHello({ name: 'Adios Mundo' }, function(err, responseA3) {
+                console.log(responseA2.message);
+                console.log(responseA3.message);
+                res.render('index', {
+                    "data": body,
+                    "respuestaApp2": responseA2.message,
+                    "respuestaApp3": responseA3.message
+                });
+            });
+        });
+    });
+});
+
+app.get('/:valor', function(req, res) {
+    user = req.params.valor;
+    request(url, function(error, response, body) {
+        body = JSON.parse(body);
+        user = req.params.id;
+        client.sayHello({ name: user }, function(err, responseA2) {
+            client2.sayHello({ name: 'App3 -> ' + user }, function(err, responseA3) {
+                console.log(responseA2.message);
+                console.log(responseA3.message);
+                res.render('index', {
+                    "data": body,
+                    "respuestaApp2": responseA2.message,
+                    "respuestaApp3": responseA3.message
+                });
+            });
         });
     });
 });
